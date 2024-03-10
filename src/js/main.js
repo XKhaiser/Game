70f40bce25dedfa967478e78b71c722684c88d7e
Zanna <andri.zanch@gmail.com>
2024-03-10 01:56:12 +0100

    aggiunte skill, sistemata grafica e logiche

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,27 @@
+import moment from 'moment';
+
 // js pagina
 $(document).ready(function(){
     $("#start").off("click").on("click", function() {
         $(".menu").fadeOut(500);
+        if ($("#riepilogo").is(":visible")) {
+            $("#riepilogo").fadeOut(500);
+        }
         $(".game").delay(510).fadeIn("slow", function () {
             activeGame = true;
+            resetVars();
             gameStart();
             buyUpgrade();
             buyAbility();
+            useSkill();
         })
     })
 
     var fps = 30;
     var viewportHeight = $(window).height();
     var viewportWidth = $(window).width();
+
+    var startDate = moment();
 
     var activeGame = false;
 
@@ -24,6 +33,7 @@ $(document).ready(function(){
 
     var money = 0;
     var magic = 0;
+    var mana = 0;
     var score = 0;
 
     var enemyCd = 45;
@@ -32,7 +42,48 @@ $(document).ready(function(){
     var enemySpeed = 15000;
     var bulSpeed = 1300;
     var closestElement;
-    var minDistance = 200;
+    var minDistance = 300;
+    
+    var upgrades = 0;
+    var skills = 0;
+
+    function resetVars() {
+        fps = 30;
+        viewportHeight = $(window).height();
+        viewportWidth = $(window).width();
+
+        startDate = moment();
+
+        heroSpeed = 45;
+        speedCounter = 0;
+        projID = 0;
+        health = 10;
+        maxHealth = 10;
+
+        money = 0;
+        magic = 0;
+        mana = 10;
+        score = 0;
+
+        enemyCd = 45;
+        cdCounter = 0;
+        eneID = 0;
+        enemySpeed = 15000;
+        bulSpeed = 1300;
+        closestElement;
+        minDistance = 300;
+
+        upgrades = 0;
+        skills = 0;
+
+        $(".money h2 span").html(parseInt(money));
+        $(".magic h2 span").html(parseInt(magic));
+
+        getDamage(true);
+
+        $(".game button").addClass("disabled");
+        $(".skills button").hide();
+    }
 
     function gameStart() {
         tick()
@@ -47,6 +98,7 @@ $(document).ready(function(){
                 enemySpawn();
                 checkHit();
                 checkDamage();
+                manaRegen();
         
             }, 1000 / fps);
         }
@@ -66,7 +118,9 @@ $(document).ready(function(){
             if (distance <= minDistance && speedCounter >= heroSpeed) {
                 closestElement = $(enemy);
 
-                heroProjectile($(enemy));
+                var distSpeed = distance / minDistance;
+
+                heroProjectile($(enemy), distSpeed);
                 speedCounter = 0;
                 return;
             }
@@ -74,7 +128,7 @@ $(document).ready(function(){
         speedCounter++;
     }
 
-    function heroProjectile(target) {
+    function heroProjectile(target, distance) {
         var startPos = {};
             startPos.top = $("#hero").position().top;
             startPos.left = $("#hero").position().left;
@@ -88,7 +142,7 @@ $(document).ready(function(){
         $("#bul" + projID).animate({
             top: endPos.top + "px",
             left: endPos.left + "px"
-        }, bulSpeed, "linear", function() {
+        }, bulSpeed * distance, "linear", function() {
             $(this).remove();
         })
 
@@ -100,8 +154,10 @@ $(document).ready(function(){
             heroSpeed = heroSpeed - (heroSpeed * 0.25);
             var cost = Number($(this).data().cost);
             money = money - cost;
+            upgrades++;
             cost = cost + (cost * 0.5);
-            $(this).data("cost", cost)
+            $(this).data("cost", cost);
+            $(this).find("cost").html(parseInt(cost));
             checkMoney()
         })
         $("#rangeBtn").off("click").on("click", function() {
@@ -109,37 +165,96 @@ $(document).ready(function(){
             $("#range").css("width", minDistance * 2 + "px").css("height", minDistance * 2 + "px");
             var cost = Number($(this).data().cost);
             money = money - cost;
+            upgrades++;
             cost = cost + (cost * 0.5);
-            $(this).data("cost", cost)
+            $(this).data("cost", cost);
+            $(this).find("cost").html(parseInt(cost));
             checkMoney()
         })
         $("#bulSpeed").off("click").on("click", function() {
             bulSpeed = bulSpeed - bulSpeed * 0.13;
             var cost = Number($(this).data().cost);
             money = money - cost;
+            upgrades++;
             cost = cost + (cost * 0.5);
-            $(this).data("cost", cost)
+            $(this).data("cost", cost);
+            $(this).find("cost").html(parseInt(cost));
             checkMoney()
         })
     }
 
     function buyAbility() {
         $("#heal").off("click").on("click", function() {
+            $("#healSkill").show();
+            var cost = Number($(this).data().cost);
+            magic = magic - cost;
+            $(this).data("cost", cost)
+            $(this).find("cost").html(parseInt(cost));
+            checkMagic();
+        })
+
+        $("#smite").off("click").on("click", function() {
+            $("#smiteSkill").show();
+            var cost = Number($(this).data().cost);
+            magic = magic - cost;
+            $(this).data("cost", cost)
+            $(this).find("cost").html(parseInt(cost));
+            checkMagic();
+        })
+    }
+
+    function useSkill() {
+        $("#healSkill").off("click").on("click", function() {
+            var _this = $(this);
+            $(_this).addClass("loading");
             $("#healEffect").fadeIn(400).delay(200).fadeOut(400);
             if (health + (health * 0.5) > maxHealth) {
                 health = maxHealth;
             } else {
                 health = health + (maxHealth * 0.5);
             }
-            var cost = Number($(this).data().cost);
-            magic = magic - cost;
-            $(this).data("cost", cost)
-            checkMagic()
+            getDamage(true);
+            mana = mana - 3;
+            skills++;
+
+            setTimeout(function(){
+                $(_this).removeClass("loading");
+            }, 6000)
+        })
+
+        $("#smiteSkill").off("click").on("click", function() {
+            var _this = $(this);
+            $(_this).addClass("loading");
+            $.each($(".enemy"), function(i, enemy) {
+                var random = Math.floor(Math.random() * (1000 - 300 + 1)) + 300;
+
+                $(enemy).delay(random).stop().fadeOut(300, function() {
+                    $(enemy).remove();
+                    enemyCd = enemyCd - enemyCd * 0.02;
+                    enemySpeed = enemySpeed - enemySpeed * 0.002;
+                    money += 1;
+                    score += 1;
+                    checkMoney();
+                    if (score % 10 === 0) {
+                        magic += 1;
+                        checkMagic();
+                    }
+                })
+            })
+
+            mana = mana - 4;
+            skills++;
+
+            setTimeout(function(){
+                $(_this).removeClass("loading");
+            }, 6000)
         })
     }
 
     function enemySpawn() {
         if (cdCounter >= enemyCd) {
+            var randomSpeed = (Math.random() * (1.3 - 0.9) + 0.9).toFixed(1);
+
             // Genera un lato casuale (0 = sinistra, 1 = destra, 2 = superiore, 3 = inferiore)
             var side = Math.floor(Math.random() * 4);
             
@@ -177,7 +292,7 @@ $(document).ready(function(){
             $("#ene" + eneID).animate({
                 top: endPos.top + "px",
                 left: endPos.left + "px"
-            }, enemySpeed, "linear", function() {
+            }, enemySpeed * randomSpeed, "linear", function() {
                 $(this).remove();
             })
             eneID++;
@@ -219,7 +334,6 @@ $(document).ready(function(){
 
     function checkMoney() {
         $.each($(".bottom #btnMoney button"), function(m, button){
-            console.log($(button).data("cost"))
             if (money < Number($(button).data("cost"))) {
                 $(button).addClass("disabled");
             } else {
@@ -231,7 +345,6 @@ $(document).ready(function(){
 
     function checkMagic() {
         $.each($(".bottom #btnMagic button"), function(m, button){
-            console.log($(button).data("cost"))
             if (magic < Number($(button).data("cost"))) {
                 $(button).addClass("disabled");
             } else {
@@ -254,13 +367,15 @@ $(document).ready(function(){
             var distance = Math.sqrt(Math.pow((heroPos.left - enemyPos.left), 2) + Math.pow((heroPos.top - enemyPos.top), 2));
             if (distance <= 20) {
                 $(enemy).remove();
-                getDamage();
+                getDamage(false);
             }
         })
     }
 
-    function getDamage() {
-        health = health -1;
+    function getDamage(heal) {
+        if (!heal) 
+            health = health -1;
+
         $("#healthValue").html(health);
         $(".health .progress-bar").css("width", (health / maxHealth) * 100 + "%");
 
@@ -272,6 +387,43 @@ $(document).ready(function(){
     function youDied() {
         activeGame = false;
 
+        var endDate = moment();
+        var time = moment.duration(endDate.diff(startDate));
+
         $(".enemy").remove();
+        $(".game").fadeOut(500);
+        $("#riepilogo").delay(501).fadeIn(500);
+
+        $("#score").html(score);
+        $("#time").html(moment.utc(time.asMilliseconds()).format('HH:mm:ss'));
+        $("#upNum").html(upgrades);
+        $("#skillNum").html(skills);
+
+        $("#retry").off("click").on("click", function() {
+            $("#start").trigger("click");
+        })
+        $("#btnMenu").off("click").on("click", function() {
+            $("#riepilogo").fadeOut(500);
+            $(".menu").delay(501).fadeIn(500);
+        })
+    }
+
+    function manaRegen() {
+        if (mana + 0.02 <= 10) {
+            mana += 0.005;
+        } else {
+            mana = 10;
+        }
+
+        $(".mana .progress-bar").css("width", (mana / 10) * 100 + "%");
+        $("#manaValue").html(parseInt(mana));
+
+        $.each($(".skills button"), function() {
+            if (mana >= $(this).data().cost) {
+                $(this).removeClass("disabled");
+            } else {
+                $(this).addClass("disabled");
+            }
+        })
     }
  });
