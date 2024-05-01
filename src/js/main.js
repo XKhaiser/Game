d4b92617395678a1d06d949162943f7af0b44ed6
Zanna <andri.zanch@gmail.com>
2024-05-01 13:07:10 +0200

    Aggiunta autenticazione google

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,5 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
-import { getAuth } from 'https://www.gstatic.com/firebasejs/10.8.1/firebase-auth.js'
 import { getFirestore, collection, doc, setDoc, getDoc } from 'https://www.gstatic.com/firebasejs/10.8.1/firebase-firestore.js'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -20,21 +19,111 @@ const firebase = initializeApp(firebaseConfig);
 
 var db = getFirestore(firebase);
 
+var YOUR_CLIENT_ID = '368381369169-v4rlubsvcj6sj6m9eoqs9hdm9ukoes9u.apps.googleusercontent.com';
+var YOUR_REDIRECT_URI = location.href.replace("/#", "");
+var fragmentString = location.hash.substring(1);
+var user;
+
+// Parse query string to see if page request is coming from OAuth 2.0 server.
+var params = {};
+var regex = /([^&=]+)=([^&]*)/g, m;
+
+while (m = regex.exec(fragmentString)) {
+    params[decodeURIComponent(m[1])] = decodeURIComponent(m[2]);
+}
+location.hash = "";
+if (Object.keys(params).length > 0) {
+    localStorage.setItem('oauth2-test-params', JSON.stringify(params) );
+    if (params['state'] && params['state'] == 'try_sample_request') {
+        trySampleRequest();
+    }
+} else {
+    trySampleRequest();
+}
+
+// If there's an access token, try an API request.
+// Otherwise, start OAuth 2.0 flow.
+function trySampleRequest() {
+    var params = JSON.parse(localStorage.getItem('oauth2-test-params'));
+    if (params && params['access_token']) {
+        fetch('https://www.googleapis.com/oauth2/v2/userinfo?' +
+        'access_token=' + params['access_token'])
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log(data);
+            user = data;
+            initMenu();
+        })
+        .catch(error => {
+            console.error('There was a problem with the fetch operation:', error);
+            $("#profile").on("click", function() {
+                oauth2SignIn(); // Prompt for user permission if token is invalid
+            }) 
+        });
+    } else {
+        $("#profile").on("click", function() {
+            oauth2SignIn();
+        })
+    }
+}
+
+/*
+* Create form to request access token from Google's OAuth 2.0 server.
+*/
+function oauth2SignIn() {
+    // Google's OAuth 2.0 endpoint for requesting an access token
+    var oauth2Endpoint = 'https://accounts.google.com/o/oauth2/v2/auth';
+
+    // Create element to open OAuth 2.0 endpoint in new window.
+    var form = document.createElement('form');
+    form.setAttribute('method', 'GET'); // Send as a GET request.
+    form.setAttribute('action', oauth2Endpoint);
+
+    // Parameters to pass to OAuth 2.0 endpoint.
+    var params = {'client_id': YOUR_CLIENT_ID,
+                    'redirect_uri': YOUR_REDIRECT_URI,
+                    'scope': 'https://www.googleapis.com/auth/userinfo.profile',
+                    'state': 'try_sample_request',
+                    'include_granted_scopes': 'true',
+                    'response_type': 'token'};
+
+    // Add form parameters as hidden input values.
+    for (var p in params) {
+        var input = document.createElement('input');
+        input.setAttribute('type', 'hidden');
+        input.setAttribute('name', p);
+        input.setAttribute('value', params[p]);
+        form.appendChild(input);
+    }
+
+    // Add form to page and submit it to open the OAuth 2.0 endpoint.
+    document.body.appendChild(form);
+    form.submit();
+}
+
+function initMenu() {
+    $("#username").html(user.given_name)
+    $("#imgUser").attr("src", user.picture)
+
+    localStorage.setItem('user', JSON.stringify(user) );
+
+    $("#start").removeClass("disabled");
+}
+
 // js pagina
 $(document).ready(function(){
     $('#modalStart').modal('show');
 
-    var user = "";
     var tutorialCompleted = false;
+    // var user = "";
 
     $("#save").off("click").on("click", function() {
-        var name = $("#userName").val();
-        if (name.length > 1) {
-            user = name;
-            $('#modalStart').modal('hide');
-        } else {
-            alert("Inserisci un nome valido!");
-        }
+        $('#modalStart').modal('hide');
     })
 
     $("#start").off("click").on("click", function() {
@@ -645,7 +734,7 @@ $(document).ready(function(){
 
             $.each(scores, function(i, point) {
                 if (i <= 9) {
-                    var html = '<li class="list-group-item d-flex justify-content-between align-items-center gap-4"><h6>' + point.user + '</h6><h6>' + point.score + '</h6></li>';
+                    var html = '<li class="list-group-item d-flex justify-content-between align-items-center gap-4"><h6>' + point.user.given_name + '</h6><h6>' + point.score + '</h6></li>';
 
                     $("#leaderboard").append(html);
                 }
@@ -697,10 +786,10 @@ $(document).ready(function(){
             noCheat();
     });
 
-    // $(window).on('blur', function(){
-    //     if (activeGame)
-    //         noCheat();
-    // });
+    $(window).on('blur', function(){
+        if (activeGame)
+            noCheat();
+    });
 
     function manaRegen() {
         if (mana + 0.02 <= 10) {
@@ -725,7 +814,7 @@ $(document).ready(function(){
         var campi = await loadTopScoresFromFirebase();
 
         // Controlla se esiste già un oggetto con la stessa proprietà "user"
-        var existingUserIndex = campi.findIndex(item => item.user.toLowerCase() === user.toLowerCase());
+        var existingUserIndex = campi.findIndex(item => item.user.id === user.id);
 
         if (existingUserIndex !== -1) {
             // Se l'utente esiste già, aggiorna il punteggio
