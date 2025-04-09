e54eb26be2a59208794424f236e618264083f04b
a.zanchettin <andri.zanch@gmail.com>
2025-04-09 23:28:39 +0200

    prova server

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,23 @@
+async function testServer () {
+    var currentUserID = 1;
+    try {
+        const response = await fetch('188.216.235.20:3000/action', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ action: 'getUserCharacters', currentUserID })
+        });
+  
+        const result = await response.json();
+
+        console.log(result);
+        
+      } catch (error) {
+        console.error('Errore:', error);
+      }
+}
+
+testServer();
+
 function addPatchNotes() {
     $.ajax({
         type: "GET",
